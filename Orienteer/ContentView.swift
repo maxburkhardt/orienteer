249bc7f8b95da8815899c3c868310d91791d4cd5
Maximilian Burkhardt <squid219@gmail.com>
2020-12-15 20:13:12 -0800

    Initial Commit

---
+++ Orienteer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Orienteer
+//
+//  Created by Maximilian Burkhardt on 12/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Orienteer/OrienteerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrienteerApp.swift
+//  Orienteer
+//
+//  Created by Maximilian Burkhardt on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct OrienteerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
